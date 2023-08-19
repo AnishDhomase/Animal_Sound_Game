6592c62825df7606d4c4cf51eb792f8a52397731
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2023-08-19 16:29:42 +0530

    Updated index.js with derectory of audio

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ function makeSound(key){
     switch (key) {
     
         case "c":
-            var sound= new Audio("sounds/cock.mp3");
+            var sound= new Audio("cock.mp3");
             sound.play();
             break;
 
         case "d":
-            var sound= new Audio("sounds/donkey.mp3");
+            var sound= new Audio("donkey.mp3");
             sound.play();
             break;
 
         case "h":
-            var sound= new Audio("sounds/horse.mp3");
+            var sound= new Audio("horse.mp3");
             sound.play();
             break;
     
         case "s":
-            var sound= new Audio("sounds/sheep.mp3");
+            var sound= new Audio("sheep.mp3");
             sound.play();
             break;
     
